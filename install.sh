23e819800e9f9ef279261886866fb5d01c7f3cc7
Jeff Benson <wolfman18336@gmail.com>
2019-03-15 00:00:52 -0400

    added install for neovim, the superior vim

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #Install Script for .dotfiles
-#probably should be root
-if [[ $EUID -ne 0 ]]; then
-	   echo "Probably should be root...just sayin'..." 
-	      exit 1
-      fi
+echo Probably should be root...just saying'
+
+#install NeoVim
+apt install nvim
 
 #install Plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
